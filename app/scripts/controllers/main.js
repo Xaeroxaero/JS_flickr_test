6132e402079dcafa418db45297217dc74f4c813d
xaeroxaero <p69hnjd1>
2017-05-24 19:51:35 +0200

    reworking

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,9 +8,37 @@
  * Controller of the jsTestApp
  */
 angular.module('jsTestApp')
-  .controller('MainCtrl', ['$scope', '$search', function ($scope, $search) {
-      $scope.search = function (search) {
-        $search.doSearch(search);
+  .controller('MainCtrl', ['$scope', '$gallery','$stateParams', function ($scope, $gallery, $stateParams) {
+
+    function setSinglePhoto(id) {
+      $scope.photos.forEach(function (photo) {
+        console.log(Number(id));
+        if (id === photo.id) {
+          $scope.photo = photo;
+          console.log(photo)
+        }
+      });
+    }
+
+
+    $scope.id = [];
+    $scope.photos = [];
+    $scope.text = '';
+    $scope.search = function (search) {
+      var promise = $gallery.getGallery(search);
+      promise.then(function (data) {
+        $scope.photos = data.data.photos.photo;
+        // console.log($scope.photos);
+        // console.log($scope.photos.id);
+        // console.log($stateParams)
+      }
+      );
+    };
+      $scope.init = function () {
+        // console.log($stateParams.id);
+        setSinglePhoto($stateParams.id)
       };
+
+      $scope.init();
   }]
   );
